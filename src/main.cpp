4a4d2911313f8c9184d3a09ce8ab8910299864f3
Arne Bersch <arne.bersch@web.de>
2025-11-06 10:27:24 +0100

    fixed Blind on startup

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    QStringList queries;
     if (fileExists) {
         QSqlQuery versionQuery;
         if (!versionQuery.exec("SELECT version FROM about")) {
@@ -62,6 +63,7 @@ int main(int argc, char *argv[]) {
             qFatal() << "Can't load this Zöglfrex file because its version (" << versionQuery.value(0).toString() << ") isn't compatible with this version of Zöglfrex (" << VERSION << ")";
             return 1;
         }
+        queries.append("UPDATE currentitems SET cue_key = NULL"); // Reset Blind
     } else {
         qInfo() << "Created File" << fileName;
         auto createItemListAttributeTable = [] (const QString tableName, const QString itemTable, const QString foreignItemTable) {
@@ -88,73 +90,71 @@ int main(int argc, char *argv[]) {
         auto createItemAndIntegerSpecificNumberAttribute = [] (const QString tableName, const QString itemTable, const QString foreignItemTable, const QString valueType) {
             return "CREATE TABLE " + tableName + " (item_key INTEGER REFERENCES " + itemTable + " (key) ON DELETE CASCADE NOT NULL, foreignitem_key INTEGER REFERENCES " + foreignItemTable + " (key) ON DELETE CASCADE NOT NULL, key INTEGER NOT NULL, value " + valueType + " DEFAULT 0 NOT NULL, PRIMARY KEY (item_key, foreignitem_key, key))";
         };
-        const QStringList createTableQueries = {
-            "PRAGMA foreign_keys = ON",
-            "CREATE TABLE models (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', channels TEXT DEFAULT 'D' NOT NULL, panrange REAL DEFAULT 540 NOT NULL, tiltrange REAL DEFAULT 270 NOT NULL, minzoom REAL DEFAULT 5 NOT NULL, maxzoom REAL DEFAULT 60 NOT NULL, PRIMARY KEY (key))",
-            "CREATE TABLE fixtures (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', model_key INTEGER REFERENCES models (key) ON DELETE SET NULL, universe INTEGER DEFAULT 1 NOT NULL, address INTEGER DEFAULT 0 NOT NULL, xposition REAL DEFAULT 0 NOT NULL, yposition REAL DEFAULT 0 NOT NULL, rotation REAL DEFAULT 0 NOT NULL, invertpan INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            "CREATE TABLE groups (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', PRIMARY KEY (key))",
-            createItemListAttributeTable("group_fixtures", "groups", "fixtures"),
-            "CREATE TABLE raws (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', movewhiledark INTEGER DEFAULT 0 NOT NULL, fade INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            createIntegerSpecificNumberAttribute("raw_channel_values", "raws", "INTEGER"),
-            createItemAndIntegerSpecificNumberAttribute("raw_model_channel_values", "raws", "models", "INTEGER"),
-            createItemAndIntegerSpecificNumberAttribute("raw_fixture_channel_values", "raws", "fixtures", "INTEGER"),
-            "CREATE TABLE intensities (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', dimmer REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            createItemSpecificNumberAttributeTable("intensity_model_dimmer", "intensities", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("intensity_fixture_dimmer", "intensities", "fixtures", "REAL"),
-            createItemListAttributeTable("intensity_raws", "intensities", "raws"),
-            "CREATE TABLE colors (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', hue REAL DEFAULT 0 NOT NULL, saturation REAL DEFAULT 0 NOT NULL, quality REAL DEFAULT 100 NOT NULL, PRIMARY KEY (key))",
-            createItemSpecificNumberAttributeTable("color_model_hue", "colors", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("color_fixture_hue", "colors", "fixtures", "REAL"),
-            createItemSpecificNumberAttributeTable("color_model_saturation", "colors", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("color_fixture_saturation", "colors", "fixtures", "REAL"),
-            createItemSpecificNumberAttributeTable("color_model_quality", "colors", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("color_fixture_quality", "colors", "fixtures", "REAL"),
-            createItemListAttributeTable("color_raws", "colors", "raws"),
-            "CREATE TABLE positions (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', pan REAL DEFAULT 0 NOT NULL, tilt REAL DEFAULT 0 NOT NULL, zoom REAL DEFAULT 15 NOT NULL, focus REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            createItemSpecificNumberAttributeTable("position_model_pan", "positions", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("position_fixture_pan", "positions", "fixtures", "REAL"),
-            createItemSpecificNumberAttributeTable("position_model_tilt", "positions", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("position_fixture_tilt", "positions", "fixtures", "REAL"),
-            createItemSpecificNumberAttributeTable("position_model_zoom", "positions", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("position_fixture_zoom", "positions", "fixtures", "REAL"),
-            createItemSpecificNumberAttributeTable("position_model_focus", "positions", "models", "REAL"),
-            createItemSpecificNumberAttributeTable("position_fixture_focus", "positions", "fixtures", "REAL"),
-            createItemListAttributeTable("position_raws", "positions", "raws"),
-            "CREATE TABLE effects (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', steps INTEGER DEFAULT 2 NOT NULL, hold REAL DEFAULT 0 NOT NULL, fade REAL DEFAULT 0 NOT NULL, phase REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            createIntegerSpecificItemAttribute("effect_step_intensities", "effects", "intensities"),
-            createIntegerSpecificItemAttribute("effect_step_colors", "effects", "colors"),
-            createIntegerSpecificItemAttribute("effect_step_positions", "effects", "positions"),
-            createIntegerSpecificItemListAttribute("effect_step_raws", "effects", "raws"),
-            createIntegerSpecificNumberAttribute("effect_step_hold", "effects", "REAL"),
-            createIntegerSpecificNumberAttribute("effect_step_fade", "effects", "REAL"),
-            createItemSpecificNumberAttributeTable("effect_fixture_phase", "effects", "fixtures", "REAL"),
-            "CREATE TABLE cuelists (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', priority INTEGER DEFAULT 100 NOT NULL, movewhiledark INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))",
-            "CREATE TABLE cues (key INTEGER, id TEXT NOT NULL, sortkey INTEGER NOT NULL, cuelist_key INTEGER REFERENCES cuelists (key) ON DELETE CASCADE, label TEXT DEFAULT '', fade REAL DEFAULT 0 NOT NULL, block INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key), UNIQUE(id, cuelist_key))",
-            createItemSpecificItemAttributeTable("cue_group_intensities", "cues", "groups", "intensities"),
-            createItemSpecificItemAttributeTable("cue_group_colors", "cues", "groups", "colors"),
-            createItemSpecificItemAttributeTable("cue_group_positions", "cues", "groups", "positions"),
-            createItemSpecificItemListAttributeTable("cue_group_raws", "cues", "groups", "raws"),
-            createItemSpecificItemListAttributeTable("cue_group_effects", "cues", "groups", "effects"),
-            "ALTER TABLE cuelists ADD COLUMN currentcue_key INTEGER REFERENCES cues (key) ON DELETE SET NULL",
-            "CREATE TABLE currentitems (group_key INTEGER REFERENCES groups (key) ON DELETE SET NULL, fixture_key INTEGER REFERENCES fixtures (key) ON DELETE SET NULL, cuelist_key INTEGER REFERENCES cuelists (key) ON DELETE SET NULL, cue_key INTEGER REFERENCES cues (key) ON DELETE SET NULL, PRIMARY KEY (group_key, fixture_key, cuelist_key, cue_key))",
-            "INSERT INTO currentitems (group_key, fixture_key, cuelist_key, cue_key) VALUES (NULL, NULL, NULL, NULL)",
-            "CREATE VIEW currentgroup_fixtures AS SELECT fixtures.* FROM fixtures, group_fixtures, currentitems WHERE group_fixtures.item_key = currentitems.group_key AND fixtures.key = group_fixtures.valueitem_key",
-            "CREATE VIEW currentfixtures AS SELECT fixtures.* FROM fixtures, currentitems, currentgroup_fixtures WHERE currentitems.fixture_key = fixtures.key OR (currentitems.fixture_key IS NULL AND currentgroup_fixtures.key = fixtures.key)",
-            "CREATE TRIGGER resetfixture_trigger AFTER UPDATE OF group_key ON currentitems BEGIN UPDATE currentitems SET fixture_key = NULL; END",
-            "CREATE VIEW currentcuelist_cues AS SELECT cues.* FROM cues, currentitems WHERE cues.cuelist_key = currentitems.cuelist_key",
-            "CREATE VIEW currentcue AS SELECT cues.* FROM cues, currentitems, cuelists WHERE currentitems.cue_key = cues.key OR (currentitems.cue_key IS NULL AND currentitems.cuelist_key = cuelists.key AND cuelists.currentcue_key = cues.key)",
-            "CREATE TRIGGER resetcue_trigger AFTER UPDATE OF cuelist_key ON currentitems BEGIN UPDATE currentitems SET cue_key = NULL; END",
-            "CREATE TRIGGER createcues_trigger AFTER INSERT ON cues BEGIN UPDATE cues SET cuelist_key = (SELECT cuelist_key FROM currentitems) WHERE id = NEW.id AND cuelist_key IS NULL; END",
-            "CREATE TABLE about (version TEXT PRIMARY KEY)",
-            "INSERT INTO about (version) VALUES ('" + VERSION + "')",
-        };
-        for (QString createTableQuery : createTableQueries) {
-            QSqlQuery query;
-            if (!query.exec(createTableQuery)) {
-                qWarning() << Q_FUNC_INFO << query.executedQuery() << query.lastError().text();
-                qFatal() << "Failed to create or modify table.";
-                return 1;
-            }
+        queries.append("PRAGMA foreign_keys = ON");
+        queries.append("CREATE TABLE models (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', channels TEXT DEFAULT 'D' NOT NULL, panrange REAL DEFAULT 540 NOT NULL, tiltrange REAL DEFAULT 270 NOT NULL, minzoom REAL DEFAULT 5 NOT NULL, maxzoom REAL DEFAULT 60 NOT NULL, PRIMARY KEY (key))");
+        queries.append("CREATE TABLE fixtures (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', model_key INTEGER REFERENCES models (key) ON DELETE SET NULL, universe INTEGER DEFAULT 1 NOT NULL, address INTEGER DEFAULT 0 NOT NULL, xposition REAL DEFAULT 0 NOT NULL, yposition REAL DEFAULT 0 NOT NULL, rotation REAL DEFAULT 0 NOT NULL, invertpan INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append("CREATE TABLE groups (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', PRIMARY KEY (key))");
+        queries.append(createItemListAttributeTable("group_fixtures", "groups", "fixtures"));
+        queries.append("CREATE TABLE raws (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', movewhiledark INTEGER DEFAULT 0 NOT NULL, fade INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append(createIntegerSpecificNumberAttribute("raw_channel_values", "raws", "INTEGER"));
+        queries.append(createItemAndIntegerSpecificNumberAttribute("raw_model_channel_values", "raws", "models", "INTEGER"));
+        queries.append(createItemAndIntegerSpecificNumberAttribute("raw_fixture_channel_values", "raws", "fixtures", "INTEGER"));
+        queries.append("CREATE TABLE intensities (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', dimmer REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append(createItemSpecificNumberAttributeTable("intensity_model_dimmer", "intensities", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("intensity_fixture_dimmer", "intensities", "fixtures", "REAL"));
+        queries.append(createItemListAttributeTable("intensity_raws", "intensities", "raws"));
+        queries.append("CREATE TABLE colors (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', hue REAL DEFAULT 0 NOT NULL, saturation REAL DEFAULT 0 NOT NULL, quality REAL DEFAULT 100 NOT NULL, PRIMARY KEY (key))");
+        queries.append(createItemSpecificNumberAttributeTable("color_model_hue", "colors", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("color_fixture_hue", "colors", "fixtures", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("color_model_saturation", "colors", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("color_fixture_saturation", "colors", "fixtures", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("color_model_quality", "colors", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("color_fixture_quality", "colors", "fixtures", "REAL"));
+        queries.append(createItemListAttributeTable("color_raws", "colors", "raws"));
+        queries.append("CREATE TABLE positions (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', pan REAL DEFAULT 0 NOT NULL, tilt REAL DEFAULT 0 NOT NULL, zoom REAL DEFAULT 15 NOT NULL, focus REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append(createItemSpecificNumberAttributeTable("position_model_pan", "positions", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_fixture_pan", "positions", "fixtures", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_model_tilt", "positions", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_fixture_tilt", "positions", "fixtures", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_model_zoom", "positions", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_fixture_zoom", "positions", "fixtures", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_model_focus", "positions", "models", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("position_fixture_focus", "positions", "fixtures", "REAL"));
+        queries.append(createItemListAttributeTable("position_raws", "positions", "raws"));
+        queries.append("CREATE TABLE effects (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', steps INTEGER DEFAULT 2 NOT NULL, hold REAL DEFAULT 0 NOT NULL, fade REAL DEFAULT 0 NOT NULL, phase REAL DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append(createIntegerSpecificItemAttribute("effect_step_intensities", "effects", "intensities"));
+        queries.append(createIntegerSpecificItemAttribute("effect_step_colors", "effects", "colors"));
+        queries.append(createIntegerSpecificItemAttribute("effect_step_positions", "effects", "positions"));
+        queries.append(createIntegerSpecificItemListAttribute("effect_step_raws", "effects", "raws"));
+        queries.append(createIntegerSpecificNumberAttribute("effect_step_hold", "effects", "REAL"));
+        queries.append(createIntegerSpecificNumberAttribute("effect_step_fade", "effects", "REAL"));
+        queries.append(createItemSpecificNumberAttributeTable("effect_fixture_phase", "effects", "fixtures", "REAL"));
+        queries.append("CREATE TABLE cuelists (key INTEGER, id TEXT UNIQUE NOT NULL, sortkey INTEGER NOT NULL, label TEXT DEFAULT '', priority INTEGER DEFAULT 100 NOT NULL, movewhiledark INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key))");
+        queries.append("CREATE TABLE cues (key INTEGER, id TEXT NOT NULL, sortkey INTEGER NOT NULL, cuelist_key INTEGER REFERENCES cuelists (key) ON DELETE CASCADE, label TEXT DEFAULT '', fade REAL DEFAULT 0 NOT NULL, block INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (key), UNIQUE(id, cuelist_key))");
+        queries.append(createItemSpecificItemAttributeTable("cue_group_intensities", "cues", "groups", "intensities"));
+        queries.append(createItemSpecificItemAttributeTable("cue_group_colors", "cues", "groups", "colors"));
+        queries.append(createItemSpecificItemAttributeTable("cue_group_positions", "cues", "groups", "positions"));
+        queries.append(createItemSpecificItemListAttributeTable("cue_group_raws", "cues", "groups", "raws"));
+        queries.append(createItemSpecificItemListAttributeTable("cue_group_effects", "cues", "groups", "effects"));
+        queries.append("ALTER TABLE cuelists ADD COLUMN currentcue_key INTEGER REFERENCES cues (key) ON DELETE SET NULL");
+        queries.append("CREATE TABLE currentitems (group_key INTEGER REFERENCES groups (key) ON DELETE SET NULL, fixture_key INTEGER REFERENCES fixtures (key) ON DELETE SET NULL, cuelist_key INTEGER REFERENCES cuelists (key) ON DELETE SET NULL, cue_key INTEGER REFERENCES cues (key) ON DELETE SET NULL, PRIMARY KEY (group_key, fixture_key, cuelist_key, cue_key))");
+        queries.append("INSERT INTO currentitems (group_key, fixture_key, cuelist_key, cue_key) VALUES (NULL, NULL, NULL, NULL)");
+        queries.append("CREATE VIEW currentgroup_fixtures AS SELECT fixtures.* FROM fixtures, group_fixtures, currentitems WHERE group_fixtures.item_key = currentitems.group_key AND fixtures.key = group_fixtures.valueitem_key");
+        queries.append("CREATE VIEW currentfixtures AS SELECT fixtures.* FROM fixtures, currentitems, currentgroup_fixtures WHERE currentitems.fixture_key = fixtures.key OR (currentitems.fixture_key IS NULL AND currentgroup_fixtures.key = fixtures.key)");
+        queries.append("CREATE TRIGGER resetfixture_trigger AFTER UPDATE OF group_key ON currentitems BEGIN UPDATE currentitems SET fixture_key = NULL; END");
+        queries.append("CREATE VIEW currentcuelist_cues AS SELECT cues.* FROM cues, currentitems WHERE cues.cuelist_key = currentitems.cuelist_key");
+        queries.append("CREATE VIEW currentcue AS SELECT cues.* FROM cues, currentitems, cuelists WHERE currentitems.cue_key = cues.key OR (currentitems.cue_key IS NULL AND currentitems.cuelist_key = cuelists.key AND cuelists.currentcue_key = cues.key)");
+        queries.append("CREATE TRIGGER resetcue_trigger AFTER UPDATE OF cuelist_key ON currentitems BEGIN UPDATE currentitems SET cue_key = NULL; END");
+        queries.append("CREATE TRIGGER createcues_trigger AFTER INSERT ON cues BEGIN UPDATE cues SET cuelist_key = (SELECT cuelist_key FROM currentitems) WHERE id = NEW.id AND cuelist_key IS NULL; END");
+        queries.append("CREATE TABLE about (version TEXT PRIMARY KEY)");
+        queries.append("INSERT INTO about (version) VALUES ('" + VERSION + "')");
+    }
+    for (QString queryText : queries) {
+        QSqlQuery query;
+        if (!query.exec(queryText)) {
+            qWarning() << Q_FUNC_INFO << query.executedQuery() << query.lastError().text();
+            qFatal() << "Failed to create or modify table.";
+            return 1;
         }
     }
 
